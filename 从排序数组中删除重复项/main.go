4efd7e8d09e4d43f95eaf2990c34ac9037fcba29
wiseacreee <wiseacreee@gmail.com>
2018-12-21 15:47:50 +0800

    init

---
+++ 从排序数组中删除重复项/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+/*
+给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ */
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	i := 0
+	for j := 1; j < len(nums); j++ {
+		if nums[i] != nums[j] {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+	return i+1
+}
+
+func main() {
+	nums := []int{0,0,1,1,1,2,2,3,3,4}
+	newLen := removeDuplicates(nums)
+	fmt.Println(newLen)
+	fmt.Println(nums)
+}
