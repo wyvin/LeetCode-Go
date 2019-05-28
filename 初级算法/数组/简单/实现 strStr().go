ab95e1a78b1625c71f4e26b749b06870193e7f08
wyvin <wyvin.cai@qq.com>
2019-05-28 18:14:15 +0800

    update

--- 初级算法/数组/简单/实现 strStr().go
+++ 初级算法/数组/简单/实现 strStr().go
@@ -18,8 +18,9 @@ import "fmt"
 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
 
 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
- */
+*/
 
+/*
 func strStr(haystack string, needle string) int {
 	if needle == "" {
 		return 0
@@ -42,9 +43,67 @@ func strStr(haystack string, needle string) int {
 	}
 	return -1
 }
+*/
 
-func main()  {
-	s := "aaa"
+// sunday
+func strStr(haystack string, needle string) int {
+	if needle == "" || haystack == needle {
+		return 0
+	}
+	if haystack == "" {
+		return -1
+	}
+	hl := []rune(haystack)
+	nl := []rune(needle)
+	hlen := len(hl)
+	nlen := len(nl)
+	if nlen >= hlen {
+		return -1
+	}
+	h := 0
+	n := 0
+	next := nlen
+
+	for n < nlen {
+		if hl[h] == nl[n] {
+			h++
+			n++
+		} else {
+			if next >= hlen {
+				return -1
+			}
+			tmp := -1
+			for i := nlen - 1; i >= 0; i-- {
+				if nl[i] == hl[next] {
+					tmp = i
+					break
+				}
+			}
+			next += nlen - tmp
+			h = next - nlen
+			if h > hlen - nlen {
+				return -1
+			}
+			n = 0
+		}
+	}
+	return h - nlen
+}
+
+func main() {
+	//s := "mississippi"
+	//n := "sippia"
+	//n := "issip"
+	//n := "pi"
+	//s := "hello"
+	//n := "ll"
+	//s := "aaaa"
+	//n := "ab"
+	//s := "aaaa"
+	//n := "aaaaa"
+	//s := "aaaaa"
+	//n := "baaaa"
+	s := ""
 	n := "a"
 	fmt.Println(strStr(s, n))
 }
