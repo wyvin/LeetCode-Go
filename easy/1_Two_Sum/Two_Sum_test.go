041be2474fd36614e0acd98ef57e90a7afa5944d
wyvin <wyvin.cai@qq.com>
2021-01-02 01:07:43 +0800

    update #1

---
+++ easy/1_Two_Sum/Two_Sum_test.go
@@ -0,0 +1,30 @@
+package __Two_Sum
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTwoSum(t *testing.T) {
+	Run(&input{
+		Nums:   []int{2, 7, 11, 15},
+		Target: 9,
+		Answer:[]int{0,1},
+	})
+}
+
+type input struct {
+	Nums []int
+	Target int
+	Answer
+}
+type Answer []int
+
+var output []int
+
+func Run(input *input) {
+	fmt.Printf("input: %+v\n", input)
+	output = twoSum1(input.Nums, input.Target)
+	fmt.Printf("output: %v\n", output)
+	fmt.Printf("answer: %v\n\n\n", input.Answer)
+}
