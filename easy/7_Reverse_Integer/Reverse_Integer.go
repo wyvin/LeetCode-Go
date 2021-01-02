fa528e4ee6ebad2d01e998a216237a611b0de3f0
wyvin <wyvin.cai@qq.com>
2021-01-02 12:38:45 +0800

    add #7

---
+++ easy/7_Reverse_Integer/Reverse_Integer.go
@@ -0,0 +1,16 @@
+package __Reverse_Integer
+
+// 2021-01-02
+func reverse(x int) int {
+	var b int
+	var res int
+	for x!=0 {
+		b = x % 10
+		x /= 10
+		res = res * 10 + b
+	}
+	if res > 2<<31-1 || res < -2<<31 {
+		return 0
+	}
+	return res
+}
---
+++ easy/7_Reverse_Integer/Reverse_Integer_test.go
@@ -0,0 +1,44 @@
+package __Reverse_Integer
+
+import (
+	"fmt"
+	"testing"
+)
+
+type input struct {
+	X int
+	Answer
+}
+type Answer int
+
+var output int
+
+func Run(input *input) {
+	fmt.Printf("input: %v\n", input.X)
+	output = reverse(input.X)
+	fmt.Printf("output: %v\n", output)
+	fmt.Printf("answer: %v\n\n", input.Answer)
+}
+
+
+func TestReverse(t *testing.T) {
+	Run(&input{
+		X: 123,
+		Answer: 321,
+	})
+
+	Run(&input{
+		X: -123,
+		Answer: -321,
+	})
+
+	Run(&input{
+		X: -120,
+		Answer: -21,
+	})
+
+	Run(&input{
+		X: 1563847412,
+		Answer: 0,
+	})
+}
