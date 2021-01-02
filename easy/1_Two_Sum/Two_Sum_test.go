5984c857f4c2eefd6d4067ccc936de73d7558e23
wyvin <wyvin.cai@qq.com>
2021-01-02 14:23:44 +0800

    update 1

--- easy/1_Two_Sum/Two_Sum_test.go
+++ easy/1_Two_Sum/Two_Sum_test.go
@@ -5,14 +5,6 @@ import (
 	"testing"
 )
 
-func TestTwoSum(t *testing.T) {
-	Run(&input{
-		Nums:   []int{2, 7, 11, 15},
-		Target: 9,
-		Answer:[]int{0,1},
-	})
-}
-
 type input struct {
 	Nums []int
 	Target int
@@ -27,4 +19,12 @@ func Run(input *input) {
 	output = twoSum1(input.Nums, input.Target)
 	fmt.Printf("output: %v\n", output)
 	fmt.Printf("answer: %v\n\n\n", input.Answer)
+}
+
+func TestTwoSum(t *testing.T) {
+	Run(&input{
+		Nums:   []int{2, 7, 11, 15},
+		Target: 9,
+		Answer:[]int{0,1},
+	})
 }
