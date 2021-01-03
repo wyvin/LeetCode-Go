a77b8a6ead594e37c7abd76d1b43ba787475b739
wyvin <wyvin.cai@qq.com>
2021-01-03 22:48:33 +0800

    update 7

--- easy/7_Reverse_Integer/Reverse_Integer.go
+++ easy/7_Reverse_Integer/Reverse_Integer.go
@@ -1,5 +1,7 @@
 package __Reverse_Integer
 
+import "math"
+
 // 2021-01-02
 func reverse(x int) int {
 	var b int
@@ -7,10 +9,14 @@ func reverse(x int) int {
 	for x!=0 {
 		b = x % 10
 		x /= 10
+		if res >math.MaxInt32/10 || (res==math.MaxInt32/10 && b>7) {
+			return 0
+		}
+		if res < math.MinInt32/10 || (res==math.MinInt32/10 && b< -8) {
+			return 0
+		}
 		res = res * 10 + b
-	}
-	if res > 2<<30-1 || res < -2<<30 {
-		return 0
+
 	}
 	return res
 }
