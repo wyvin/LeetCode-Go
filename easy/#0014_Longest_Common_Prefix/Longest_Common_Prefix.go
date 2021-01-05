d2ddcf0b5e6f28f49463bc0dd20568a68ee8c998
wyvin <wyvin.cai@qq.com>
2021-01-05 19:58:06 +0800

    add 14

---
+++ easy/#0014_Longest_Common_Prefix/Longest_Common_Prefix.go
@@ -0,0 +1,31 @@
+package _0014_Longest_Common_Prefix
+
+import (
+	"math"
+)
+
+// 2021-01-05
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+
+	prefix := []byte(strs[0])
+	var str []byte
+	var length int
+	for i, _ := range strs {
+		if strs[i] == "" {
+			return ""
+		}
+		str = []byte(strs[i])
+		length = int(math.Min(float64(len(str)), float64(len(prefix))))
+		prefix = prefix[:length]
+		for j:=0; j<length;j++ {
+			if str[j]!= prefix[j] {
+				prefix = prefix[:j]
+				break
+			}
+		}
+	}
+	return string(prefix)
+}
---
+++ easy/#0014_Longest_Common_Prefix/Longest_Common_Prefix_test.go
@@ -0,0 +1,38 @@
+package _0014_Longest_Common_Prefix
+
+import (
+	"fmt"
+	"testing"
+)
+
+type input struct {
+	X []string
+
+	Answer string
+}
+
+var output string
+
+func Run(input *input) {
+	fmt.Printf("input: %v\n", input.X)
+	output = longestCommonPrefix(input.X)
+	fmt.Printf("output: %v\n", output)
+	fmt.Printf("answer: %v\n\n", input.Answer)
+}
+
+func TestReverse(t *testing.T) {
+	Run(&input{
+		X:      []string{"flower","flow","flight"},
+		Answer: "fl",
+	})
+
+	Run(&input{
+		X:      []string{"dog","racecar","car"},
+		Answer: "",
+	})
+
+	Run(&input{
+		X:      []string{"ab","a"},
+		Answer: "a",
+	})
+}
