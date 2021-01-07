44eadfea4ac479ee17865b13841e7797979dadc7
wyvin <wyvin.cai@qq.com>
2021-01-07 21:45:01 +0800

    update 21

--- easy/#0021_Merge_Two_Sorted_Lists/Merge_Two_Sorted_Lists.go
+++ easy/#0021_Merge_Two_Sorted_Lists/Merge_Two_Sorted_Lists.go
@@ -39,3 +39,23 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	}
 	return head.Next
 }
+
+// 递归
+func mergeTwoLists2(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	var res *ListNode
+
+	if l1.Val > l2.Val {
+		res = l2
+		res.Next = mergeTwoLists(l1, l2.Next)
+	} else {
+		res = l1
+		res.Next = mergeTwoLists(l2, l1.Next)
+	}
+	return res
+}
