b7a5d3655806022b5164473f7c22eac93607c86a
wyvin <wyvin.cai@qq.com>
2021-01-11 21:15:40 +0800

    add 35

---
+++ easy/#0035_Search_Insert_Position/Search_Insert_Position.go
@@ -0,0 +1,30 @@
+package _0035_Search_Insert_Position
+
+// 2021-01-11
+func searchInsert(nums []int, target int) int {
+	for i, _ := range nums {
+		if nums[i] >= target {
+			return i
+		}
+	}
+	return len(nums)
+}
+
+// 二分查找
+func searchInsert2(nums []int, target int) int {
+	n := len(nums)
+	l,h := 0, n-1
+	res,mid := n,0
+	for l <= h {
+		mid = (l+h)>>1
+		// 不能用>=比较，因为会出现h+1=n的答案
+		// 循环的条件内得不到h+1
+		if target <= nums[mid] {
+			res = mid
+			h = mid-1
+		} else {
+			l = mid+1
+		}
+	}
+	return res
+}
---
+++ easy/#0035_Search_Insert_Position/Search_Insert_Position_test.go
@@ -0,0 +1,46 @@
+package _0035_Search_Insert_Position
+
+
+import (
+	"fmt"
+	"testing"
+)
+
+type input struct {
+	X []int
+	Target int
+
+	Answer int
+}
+
+var output int
+
+func Run(input *input) {
+	fmt.Printf("input: %v, target: %d \n", input.X,input.Target)
+	output = searchInsert2(input.X, input.Target)
+	fmt.Printf("output: %d\n", output)
+	fmt.Printf("answer: %d\n\n", input.Answer)
+}
+
+func TestReverse(t *testing.T) {
+	Run(&input{
+		X:      []int{1, 3, 5, 6},
+		Target: 5,
+		Answer: 2,
+	})
+	Run(&input{
+		X:      []int{1, 3, 5, 6},
+		Target: 2,
+		Answer: 1,
+	})
+	Run(&input{
+		X:      []int{1, 3, 5, 6},
+		Target: 7,
+		Answer: 4,
+	})
+	Run(&input{
+		X:      []int{1, 3, 5, 6},
+		Target: 0,
+		Answer: 0,
+	})
+}
