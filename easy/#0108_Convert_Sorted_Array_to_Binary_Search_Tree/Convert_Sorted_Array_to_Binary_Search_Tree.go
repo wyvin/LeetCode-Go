08a861d1be74c6fd2e97a1b0b39510d00f417db8
wyvin <wyvin.cai@qq.com>
2021-01-25 18:13:20 +0800

    update 108

--- easy/#0108_Convert_Sorted_Array_to_Binary_Search_Tree/Convert_Sorted_Array_to_Binary_Search_Tree.go
+++ easy/#0108_Convert_Sorted_Array_to_Binary_Search_Tree/Convert_Sorted_Array_to_Binary_Search_Tree.go
@@ -6,6 +6,8 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// 2021-01-24
+// 递归
 func sortedArrayToBST(nums []int) *TreeNode {
 	return helper(nums, 0, len(nums)-1)
 }
@@ -19,4 +21,53 @@ func helper(nums []int, left, right int) *TreeNode {
 	root.Left = helper(nums, left, mid-1)
 	root.Right = helper(nums, mid+1, right)
 	return root
+}
+
+// 2021-01-25
+// 迭代
+func sortedArrayToBST1(nums []int) *TreeNode {
+	switch len(nums) {
+	case 0:
+		return nil
+	case 1:
+		return &TreeNode{Val: nums[0]}
+	case 2:
+		return &TreeNode{
+			Val:  nums[1],
+			Left: &TreeNode{Val: nums[0]},
+		}
+	case 3:
+		return &TreeNode{
+			Val:   nums[1],
+			Left:  &TreeNode{Val: nums[0]},
+			Right: &TreeNode{Val: nums[2]},
+		}
+	default:
+		left, right := 0, len(nums)-1
+		queue := [][]int{{left, right}}
+		root := new(TreeNode)
+		nodeQueue := []*TreeNode{root}
+		for len(queue) > 0 {
+			l := len(queue)
+			for i := 0; i < l; i++ {
+				r := queue[i]
+				left, right = r[0], r[1]
+				mid := (left + right) / 2
+				nodeQueue[i].Val = nums[mid]
+				if left < mid {
+					queue = append(queue, []int{left, mid - 1})
+					nodeQueue[i].Left = &TreeNode{}
+					nodeQueue = append(nodeQueue, nodeQueue[i].Left)
+				}
+				if right > mid {
+					queue = append(queue, []int{mid + 1, right})
+					nodeQueue[i].Right = &TreeNode{}
+					nodeQueue = append(nodeQueue, nodeQueue[i].Right)
+				}
+			}
+			queue = queue[l:]
+			nodeQueue = nodeQueue[l:]
+		}
+		return root
+	}
 }
