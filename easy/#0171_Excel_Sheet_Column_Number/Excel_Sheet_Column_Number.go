7b24aff6977ac545489b2c4329ab425160ea67c4
wyvin <wyvin.cai@qq.com>
2021-02-16 23:52:06 +0800

    update 171

--- easy/#0171_Excel_Sheet_Column_Number/Excel_Sheet_Column_Number.go
+++ easy/#0171_Excel_Sheet_Column_Number/Excel_Sheet_Column_Number.go
@@ -1,1 +1,25 @@
 package _0171_Excel_Sheet_Column_Number
+
+import "math"
+
+// 从后开始算
+func titleToNumber(s string) int {
+	bs := []byte(s)
+	sum := 0
+	bsl := len(bs)
+	for i := bsl - 2; i >= 0; i-- {
+		sum += (int(bs[i]-'A') + 1) * int(math.Pow(26, float64(bsl-1-i)))
+	}
+	sum += int(bs[bsl-1]-'A') + 1
+	return sum
+}
+
+// 从前开始算
+func titleToNumber1(s string) int {
+	sum := 0
+	bs := []byte(s)
+	for _, b := range bs {
+		sum = 26*sum + (int(b-'A') + 1)
+	}
+	return sum
+}
--- easy/#0171_Excel_Sheet_Column_Number/Excel_Sheet_Column_Number_test.go
+++ easy/#0171_Excel_Sheet_Column_Number/Excel_Sheet_Column_Number_test.go
@@ -1,1 +1,46 @@
 package _0171_Excel_Sheet_Column_Number
+
+import (
+	"fmt"
+	"math"
+	"testing"
+)
+
+type input struct {
+	X string
+
+	Answer int
+}
+
+func Run(input *input) {
+	fmt.Printf("input: %v\n", input.X)
+	output := titleToNumber(input.X)
+	fmt.Printf("output: %+v\n", output)
+	fmt.Printf("answer: %+v\n\n", input.Answer)
+	if fmt.Sprintf("%+v", input.Answer) != fmt.Sprintf("%+v", output) {
+		panic("解答错误")
+	}
+}
+
+func Test(t *testing.T) {
+	Run(&input{
+		X:      "A",
+		Answer: 1,
+	})
+	Run(&input{
+		X:      "AB",
+		Answer: 28,
+	})
+	Run(&input{
+		X:      "ZY",
+		Answer: 701,
+	})
+	Run(&input{
+		X:      "AZ",
+		Answer: 52,
+	})
+	Run(&input{
+		X:      "AAA",
+		Answer: 703,
+	})
+}
