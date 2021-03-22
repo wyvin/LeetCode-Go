b5cc2189e12f491f0e6932cd05335e46718d6b85
wyvin <wyvin.cai@qq.com>
2021-03-22 11:07:44 +0800

    udpate 225

--- easy/#0225_Implement_Stack_using_Queues/Implement_Stack_using_Queues.go
+++ easy/#0225_Implement_Stack_using_Queues/Implement_Stack_using_Queues.go
@@ -1,1 +1,51 @@
 package _0225_Implement_Stack_using_Queues
+
+// 类似汉诺塔的思路
+type MyStack struct {
+	ListIn  []int
+	ListOut []int
+}
+
+/** Initialize your data structure here. */
+func Constructor() MyStack {
+	return MyStack{
+		ListIn:  []int{},
+		ListOut: []int{},
+	}
+}
+
+/** Push element x onto stack. */
+func (this *MyStack) Push(x int) {
+	this.ListIn = append(this.ListIn, x)
+	if len(this.ListOut) != 0 {
+		this.ListIn = append(this.ListIn, this.ListOut...)
+		this.ListOut = []int{}
+	}
+	this.ListIn, this.ListOut = this.ListOut, this.ListIn
+}
+
+/** Removes the element on top of the stack and returns that element. */
+func (this *MyStack) Pop() int {
+	ans := this.ListOut[0]
+	this.ListOut = this.ListOut[1:]
+	return ans
+}
+
+/** Get the top element. */
+func (this *MyStack) Top() int {
+	return this.ListOut[0]
+}
+
+/** Returns whether the stack is empty. */
+func (this *MyStack) Empty() bool {
+	return len(this.ListIn) == 0 && len(this.ListOut) == 0
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.Empty();
+ */
--- easy/#0225_Implement_Stack_using_Queues/Implement_Stack_using_Queues_test.go
+++ easy/#0225_Implement_Stack_using_Queues/Implement_Stack_using_Queues_test.go
@@ -1,1 +1,63 @@
 package _0225_Implement_Stack_using_Queues
+
+import (
+	"fmt"
+	"testing"
+)
+
+type input struct {
+	X []string
+	Y [][]int
+
+	Answer []interface{}
+}
+
+var myStack MyStack
+
+func Run(input *input) {
+	fmt.Printf("input: \n")
+	fmt.Printf("%v \n", input.X)
+	fmt.Printf("%v \n", input.Y)
+
+	selector := func(a string, b []int) interface{} {
+		switch a {
+		case "MyStack":
+			myStack = Constructor()
+		case "push":
+			for _, v := range b {
+				myStack.Push(v)
+			}
+		case "top":
+			return myStack.Top()
+		case "pop":
+			return myStack.Pop()
+		case "empty":
+			return myStack.Empty()
+		}
+		return nil
+	}
+
+	output := make([]interface{}, 0)
+	for i, _ := range input.X {
+		output = append(output, selector(input.X[i], input.Y[i]))
+	}
+
+	fmt.Printf("output: %+v\n", output)
+	fmt.Printf("answer: %+v\n\n", input.Answer)
+	if fmt.Sprintf("%+v", input.Answer) != fmt.Sprintf("%+v", output) {
+		panic("解答错误")
+	}
+}
+
+func Test(t *testing.T) {
+	Run(&input{
+		X:      []string{"MyStack", "push", "push", "top", "pop", "empty"},
+		Y:      [][]int{{}, {1}, {2}, {}, {}, {}},
+		Answer: []interface{}{nil, nil, nil, 2, 2, false},
+	})
+	Run(&input{
+		X:      []string{"MyStack", "push", "pop", "empty"},
+		Y:      [][]int{{}, {1}, {}, {}},
+		Answer: []interface{}{nil, nil, 1, true},
+	})
+}
